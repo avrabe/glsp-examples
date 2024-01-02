eceda2103c0cd015297b4bd2faf2e102ea113394
Ralf Anton Beier <ralf_beier@me.com>
2024-01-02 18:26:09 +0100

    Created component to hold the example
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- project-templates/node-json-vscode/crates/graph/src/lib.rs
+++ project-templates/node-json-vscode/crates/graph/src/lib.rs
@@ -1,37 +1,98 @@
 cargo_component_bindings::generate!();
+use std::cell::RefCell;
 
-use bindings::Guest;
-//use uuid::Uuid;
-use crate::bindings::exports::component::graph::tasklist;
-use crate::bindings::exports::component::graph::tasklist::{Task, Position};
-struct Component;
+use crate::bindings::exports::component::tasklist::tasklist::{
+    GuestTasklistModel, OwnTasklistModel, Position, Size, Task, Transition,
+};
+use cargo_component_bindings::rt::Resource;
 
-impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
-    }
-
-    fn validate_task_name(name: String) -> bool {
-        name.len() > 0
-    }
+pub struct TasklistModel {
+    id: String,
+    tasks: RefCell<Vec<Task>>,
+    transitions: RefCell<Vec<Transition>>,
 }
-
-impl tasklist::Guest for Component {
-    fn tasks() -> Vec<Task> {
-        vec![]
+impl GuestTasklistModel for TasklistModel {
+    fn id(&self) -> String {
+        self.id.clone()
     }
 
-    fn add_task(position: Position) -> Task {
-        let id = "ddd".to_string();
-        Task {
-            id,
-            name: "task".to_string(),
+    fn tasks(&self) -> Vec<Task> {
+        self.tasks.borrow_mut().clone()
+    }
+
+    fn transitions(&self) -> Vec<Transition> {
+        self.transitions.borrow_mut().clone()
+    }
+
+    fn add_task(&self, position: Position) -> Task {
+        let task = Task {
+            id: uuid::Uuid::new_v4().to_string(),
+            name: "task-name".to_string(),
             position,
             size: None,
+        };
+        self.tasks.borrow_mut().push(task.clone());
+        task
+    }
+
+    fn remove_task(&self, id: String) {
+        if let Some(index) = self.tasks.borrow_mut().iter().position(|t| t.id == id) {
+            self.tasks.borrow_mut().remove(index);
+        }
+
+        self.transitions
+            .borrow_mut()
+            .retain(|t| t.source_task_id != id && t.target_task_id != id);
+    }
+
+    fn resize_task(&self, id: String, size: Size) {
+        if let Some(task) = self.tasks().iter_mut().find(|t| t.id == id) {
+            task.size = Some(size);
         }
     }
+
+    fn move_task(&self, id: String, position: Position) {
+        if let Some(task) = self.tasks().iter_mut().find(|t| t.id == id) {
+            task.position = position;
+        }
+    }
+
+    fn add_transition(&self, source: String, target: String) -> Transition {
+        let transition = Transition {
+            id: uuid::Uuid::new_v4().to_string(),
+            source_task_id: source,
+            target_task_id: target,
+        };
+        self.transitions.borrow_mut().push(transition.clone());
+        transition
+    }
+
+    fn remove_transition(&self, id: String) {
+        let mut removed_count = 0;
+        self.transitions.borrow_mut().retain(|t| {
+            if t.source_task_id == id || t.target_task_id == id {
+                removed_count += 1;
+                false
+            } else {
+                true
+            }
+        });
+    }
+
+    fn export_to_json(&self) -> String {
+        todo!()
+    }
+
+    fn import_from_json(&self, _: String) {
+        todo!()
+    }
+
+    fn create_model_for_empty_file() -> OwnTasklistModel {
+        let tasklistmodel: TasklistModel = TasklistModel {
+            id: uuid::Uuid::new_v4().to_string(),
+            tasks: vec![].into(),
+            transitions: vec![].into(),
+        };
+        Resource::<TasklistModel>::new(tasklistmodel)
+    }
 }
-
-
-
